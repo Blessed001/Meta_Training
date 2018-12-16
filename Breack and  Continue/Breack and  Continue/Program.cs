e540502232ddf23f88464d8e8b81f26ef90301f0
diotec <claudiojoc46@mail.ru>
2018-12-16 23:44:53 +0300

    Break and Continue

---
+++ Breack and  Continue/Breack and  Continue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Breack_and__Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            // Breaking clicle
+            for(int  i = 0; i < n; i++)
+            {
+                if (i == 7)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("=====================================================");
+            //Continue cicle
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 7)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
