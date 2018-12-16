1693e42313a94171fccd318796638771bad25e7f
diotec <claudiojoc46@mail.ru>
2018-12-16 23:21:43 +0300

    Basic data type conversions

---
+++ Basic data typr conversions/Basic data typr conversions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basic_data_typr_conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 4;
+            byte b = (byte)(a + 70);
+
+            ushort a1 = 4;
+            byte b1 = (byte)a1;
+
+            Console.WriteLine(b);
+            Console.WriteLine(b1);
+            Console.ReadKey();
+        }
+    }
+}
