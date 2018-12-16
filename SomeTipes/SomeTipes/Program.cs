c627884fc8f5557fa278bb8c39cad611ae23eb8e
diotec <claudiojoc46@mail.ru>
2018-12-16 21:50:38 +0300

    Some data tipes

---
+++ SomeTipes/SomeTipes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SomeTipes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Some tipes 
+            string name = "Tom";
+            int age = 33;
+            bool isEmployed = false;
+            double weight = 78.65;
+
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Возраст: {age}");
+            Console.WriteLine($"Вес: {weight}");
+            Console.WriteLine($"Работает: {isEmployed}");
+        }
+    }
+}
